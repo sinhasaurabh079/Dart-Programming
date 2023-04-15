e063f919e55dbbbca13e21d06681597768408db4
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-04-16 00:42:36 +0530

    Create image-add.dart

---
+++ image-add.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Hello'),
+      ),
+      body: Container(
+        width :100,
+        height:100,
+        child: Center(child: Image.asset('assets/images/logo.png'))),
+    
+    );
+  }
+}
