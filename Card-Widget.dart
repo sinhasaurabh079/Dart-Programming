7c2f947658acd2c68ff33d81204ab65a5167deec
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-04-17 01:53:07 +0530

    Update Card-Widget.dart

--- Card-Widget.dart
+++ Card-Widget.dart
@@ -1,1 +1,58 @@
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+       theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange.shade800,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Hello'),
+      ),
+      // Card Widget is used to give 3d effect to element
+      body: Center(
+        child: Card(
+          showdowColor: Colors.orange,
+          elevation: 05,
+            child: Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: Text(
+                 'Hello World!',
+                 style: TextStyle(fontSize: 25),
+              ),
+            )
+          ),
+        )
+    );
+  }
+}
