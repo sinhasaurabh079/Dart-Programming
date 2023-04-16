d11b31fbe6dab3a0a6492b9aafcd72c31ee005f6
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-04-16 23:40:19 +0530

    Create margin-vs-padding.dart

---
+++ margin-vs-padding.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+// main func of our app that calls runApp that further calls homepage
+void main() {
+  runApp(FlutterApp());
+}
+
+// each flutter project has a cycle that to be followed
+// hence we inherit some property from StatelessWidget or StatefulWidget depending on need
+// we need to override the default application with our own thats why we use @override
+class FlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // since flutterapp returns an application that why we return MaterialApp for android
+    // Cupertino for ios
+    return MaterialApp(
+      title: "FlutterApp",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.amber),
+      // calls homescreen the first page of our app
+      home: DashBoardScreen(),
+    );
+  }
+}
+
+// defination of DashBoardScreen
+class DashBoardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+      ),
+      // body: Padding(
+      //   // EdgeInsets.all() gives padding from al 4 sids
+      //   // EdgeInsets.only() is used when padding need to be given from particular sides
+      //   padding: const EdgeInsets.all(8.0),
+      //   child: Text(
+      //     'Namaste Guruji',
+      //     style: TextStyle(fontSize: 25),
+      //   ),
+      // )
+      // --------------
+      // by default we dont have any Margin widet but we can use it within a container widget
+      body: Container(
+        margin: EdgeInsets.all(11),
+        child: Padding(
+          // within container spacing allowed by padding
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            'Namaste Guruji',
+            style: TextStyle(fontSize: 25),
+          ),
+        ),
+      ),
+    );
+  }
+}
