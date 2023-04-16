d07ad23e69f6c0f9e0dd7cbc4938d7af68a07187
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-04-17 01:20:31 +0530

    Create util.dart

---
+++ util.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+TextStyle mTextstyle11({Color textColor=Colors.black, FontWeight fontWeight = FontWeight.normal}) {
+  return TextStyle(
+    fontSize: 11,
+    color: textColor,
+    fontWeight: fontWeight,
+  );
+}
+
+TextStyle mTextstyle21() {
+  return TextStyle(
+    fontSize: 21,
+  );
+}
+
+TextStyle mTextstyle31() {
+  return TextStyle(
+    fontSize: 31,
+  );
+}
+
+TextStyle mTextstyle41() {
+  return TextStyle(
+    fontSize: 41,
+  );
+}
+
