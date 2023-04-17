b66607a88919ce88a10dfd95e2018f7f63135333
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-04-17 20:33:38 +0530

    Update Custom-font-addition.dart

--- Custom-font-addition.dart
+++ Custom-font-addition.dart
@@ -48,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
       //     backgroundColor: Colors.blueAccent ,
       //     backgroundImage: AssetImage('assets/images/logo.png'),
       //     ),
-      // ),
+      // ), 
       body: Center(
         child: Text(
           'Namaste Guruji',
