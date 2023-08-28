318ab91a8798b059189bb3fbf7e241872ec83a12
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-08-28 21:22:51 +0530

    Create HttpRequest.dart

---
+++ HttpRequest.dart
@@ -0,0 +1,11 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() async{
+  // futures 
+  var url = Uri.https('jsonplaceholder.typicode.com', 'users/1'); 
+  final res = await http.get(url);
+  
+  print(jsonDecode(res.body)['name']);
+  
+}
